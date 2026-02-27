39313356b66ebdac6763e1b435caacc3b99e8ab1
cavalcantecpaulo <pauloc1807@hotmail.com>
2026-02-26 22:05:55 -0300

    feat: implementação completa de modularização and pequenas correções de texto

--- crud_alunos.py
+++ crud_alunos.py
@@ -1,23 +1,42 @@
 lista_alunos = []
 opc = -1
 
-def menu_inicial():
-    print("\n--- CRUD Alunos Python ---")
-    print("--- Seja bem-vindo ao Mini-Crud em Python ---")
-    print("   1 - Adicionar aluno")
-    print("   2 - Atualizar aluno")
-    print("   3 - Excluir aluno")
-    print("   4 - Exibir aluno")
-    print("   5 - Exibir todos os alunos")
-    print("   0 - Encerrar Programa")
+def menu_inicial(opc):
+    while opc != 0:
+        print("\n--- CRUD Alunos Python ---")
+        print("--- Seja bem-vindo ao Mini-Crud em Python ---")
+        print("   1 - Adicionar aluno")
+        print("   2 - Atualizar aluno")
+        print("   3 - Excluir aluno")
+        print("   4 - Exibir aluno")
+        print("   5 - Exibir todos os alunos")
+        print("   0 - Encerrar Programa")
+
+        opc = int(input("Selecione uma opção: "))
+        match opc:
+            case 1:
+                adicionar_aluno()
+            case 2:
+                atualizar_aluno()
+            case 3:
+                excluir_aluno()
+            case 4:
+                exibir_aluno()
+            case 5:
+                exibir_alunos()
+            case 0:
+                print("\nEncerrando programa...")
+                break
+            case _:
+                print("\nOpção inválida!!")
 
 def informacoes_aluno(aluno):
-    print("\n------------------------------")
-    print(f"Nome: {aluno["nome_aluno"]}")
-    print(f"RM do aluno: {aluno["rm"]}")
-    print(f"Curso: {aluno["curso"]}")
-    print(f"Valor da Mensalidade: R${aluno["mensalidade"]}")
-    print("------------------------------")
+    print("\n-----------------------------------")
+    print(f"    Nome: {aluno["nome_aluno"]}")
+    print(f"    RM do aluno: {aluno["rm"]}")
+    print(f"    Curso: {aluno["curso"]}")
+    print(f"    Valor da Mensalidade: R${aluno["mensalidade"]}")
+    print("-----------------------------------")
 
 def adicionar_aluno():
     nome = input("Digite o nome do novo aluno: ")
@@ -32,7 +51,7 @@ def adicionar_aluno():
         "mensalidade": valor_mensalidade
     }
     lista_alunos.append(aluno)
-    print("Aluno foi adicionado com sucesso!")
+    print("\nAluno foi adicionado com sucesso!")
 
 def atualizar_aluno():
     rmDigitado = requisicao_aluno("atualizar")
@@ -51,31 +70,32 @@ def atualizar_aluno():
             aluno["curso"] = curso
             aluno["mensalidade"] = valor_mensalidade
 
-            print("Informações de aluno atualizada com sucesso!")
+            print("\nInformações de aluno atualizada com sucesso!")
         else:
-            print("RM não encontrado!!!!")
+            print("\nRM não encontrado!!!!")
 
 def excluir_aluno():
     rmDigitado = requisicao_aluno("excluir")
     for aluno in lista_alunos:
         if (aluno["rm"] == rmDigitado):
             lista_alunos.pop(aluno)
-            print("Aluno excluído com sucesso!")
+            print("\nAluno excluído com sucesso!")
         else:
-            print("RM não encontrado!!!!")
+            print("\nRM não encontrado!!!!")
+
 def exibir_aluno():
     rmDigitado = requisicao_aluno("exibir")
     for aluno in lista_alunos:
         if (aluno["rm"] == rmDigitado):
             informacoes_aluno(aluno)
         else:
-            print("RM não encontrado!!!!")
+            print("\nRM não encontrado!!!!")
 
 def exibir_alunos():
     if (len(lista_alunos) == 0):
         print("\nNenhum aluno encontrado!!!!")
     else:
-        print("Exibindo alunos: ")
+        print("\nExibindo alunos: ")
     for aluno in lista_alunos:
         informacoes_aluno(aluno)
 
@@ -83,22 +103,4 @@ def requisicao_aluno(acao):
     rm = input(f"Digite o RM do aluno que deseja {acao}: ")
     return rm
 
-while (opc != 0):
-    menu_inicial()
-    opc = int(input("Selecione uma opção: "))
-    match opc:
-        case 1:
-            adicionar_aluno()
-        case 2:
-            atualizar_aluno()
-        case 3:
-            excluir_aluno()
-        case 4:
-            exibir_aluno()
-        case 5:
-            exibir_alunos()
-        case 0:
-            print("\nEncerrando programa...")
-            break
-        case _:
-            print("Opção inválida!!")
+menu_inicial(opc)
